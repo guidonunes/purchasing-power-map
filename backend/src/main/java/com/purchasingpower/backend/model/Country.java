a7c6a8a07f1ced12eb548662769e9d5cc32b096c
Guilherme Nunes <guilherme.augd@gmail.com>
2026-02-10 16:29:55 -0300

    Add: model with lombok annotations

--- backend/src/main/java/com/purchasingpower/backend/model/Country.java
+++ backend/src/main/java/com/purchasingpower/backend/model/Country.java
@@ -1,5 +1,31 @@
 package com.purchasingpower.backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@Table(name = "countries")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name="currency_code", nullable=false)
+    private String currencyCode;
+
+    @OneToMany(mappedBy="country", cascade = CascadeType.ALL)
+    private List<City> cities;
 }
